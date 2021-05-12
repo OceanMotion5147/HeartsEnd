35a3a6be56e921318ca6fe074494b61d02da4a0b
OceanMotion5147 <aidbro36@kingshigh.school.nz>
2021-05-13 10:01:19 +1200

    Autotiles Setup Complete

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,23 +1,23 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const GRAVITY = 10
+const GRAVITY = 9.80665
 const SPEED = 200
-const JUMP_HEIGHT = -500
+const JUMP_HEIGHT = -466
 var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += GRAVITY
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("Right"):
 		motion.x = SPEED
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("Left"):
 		motion.x = -SPEED
 	else:
 		motion.x = 0
 		
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_just_pressed("Up"):
 			motion.y = JUMP_HEIGHT
 			
 	motion = move_and_slide(motion,UP)
