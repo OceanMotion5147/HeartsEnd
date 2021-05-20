8e2b6ee09558b0739b1fa861028555d6821dae82
OceanMotion5147 <aidbro36@kingshigh.school.nz>
2021-05-21 09:57:34 +1200

    Smooth Movement, Props Added, Collisions Fixed

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,29 +2,35 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 9.80665
-const SPEED = 100
+const MAX_SPEED = 100
+const ACCELERATION = 50
 const JUMP_HEIGHT = -200
 var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += GRAVITY
+	var friction = false
 	
 	if Input.is_action_pressed("Right"):
-		motion.x = SPEED
+		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 	elif Input.is_action_pressed("Left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else:
-		motion.x = 0
+		motion.x = lerp(motion.x, 0, 0.2)
+		friction = true
 		$Sprite.play("Idle")
 	if is_on_floor():
 		if Input.is_action_just_pressed("Up"):
 			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
 		$Sprite.play("Jump")
-			
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 	motion = move_and_slide(motion,UP)
 	pass
