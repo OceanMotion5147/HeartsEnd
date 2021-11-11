39bfd3dfd734980a23432b7f190ccb37a81556ae
OceanMotion5147 <aidbro36@kingshigh.school.nz>
2021-11-12 10:36:16 +1300

    parallax background fixed

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -3,11 +3,9 @@ extends CanvasLayer
 var coins = 0
 var lives = 3
 
-# Shows the players score and lives on screen
+# Shows the players score on screen
 func _ready():
 	$AmmoCount/Ammo.text = String(coins)
-	$LifeCount/Life.text = String(lives)
-	
 
 
 # When player collides with ammo tin, counter goes up 1
